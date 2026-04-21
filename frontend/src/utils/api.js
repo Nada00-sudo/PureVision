492a721a6458597a239458eb8a615152edd02379
Nada EL OUKILI <eloukilinada2004@gmail.com>
2026-04-21 20:24:57 +0000

    exp1

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.100.10/api', // L'URL de notre backend
+  baseURL: '/api', // L'URL de notre backend
 });
 
 // Intercepteur pour ajouter le token JWT à chaque requête sortante
